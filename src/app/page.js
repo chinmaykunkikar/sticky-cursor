4b428c7bcac29f9a807aa4e2470e1e7b5691b042
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-27 00:16:58 +0530

    app: Create a basic StickyCursor component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 "use client";
 import Header from "@/components/header";
 import styles from "./page.module.scss";
+import StickyCursor from "@/components/stickyCursor";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
+      <StickyCursor />
     </main>
   );
 }
---
+++ src/components/stickyCursor/index.jsx
@@ -0,0 +1,5 @@
+import styles from "./style.module.scss";
+
+export default function StickyCursor() {
+  return <div className={styles.cursor}></div>;
+}
