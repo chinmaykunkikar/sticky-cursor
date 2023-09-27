1e958e187eea99422077de1b73fe0bd8d452b644
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-27 18:46:37 +0530

    StickyCursor: "manage" -> "handle"; Add comments; Standardize names

--- src/components/stickyCursor/index.jsx
+++ src/components/stickyCursor/index.jsx
@@ -6,8 +6,11 @@ export default function StickyCursor({ stickyElement }) {
   const [hovered, setHovered] = useState(false);
 
   const cursorSize = hovered ? 60 : 20; // Same as the cursor size in scss file
+
+  // For framer to recognize motiona values, we use the useMotionValue hook.
   const mouse = { x: useMotionValue(0), y: useMotionValue(0) };
 
+  // To make the sticky cursor to smoothly follow the pointer
   const smoothOpts = { damping: 20, stiffness: 300, mass: 0.3 };
   const smoothMouse = {
     x: useSpring(mouse.x, smoothOpts),
@@ -15,34 +18,36 @@ export default function StickyCursor({ stickyElement }) {
   };
 
   useEffect(() => {
-    function manageMouseMove(event) {
+    function handleMouseMove(event) {
       const { clientX, clientY } = event;
       const { left, top, height, width } =
         stickyElement.current.getBoundingClientRect();
+
+      // Calculate the center of the burger menu.
       const center = { x: left + width / 2, y: top + height / 2 };
+
       if (hovered) {
+        // Set the sticky cursor to the center of the burger
+        // when the pointer is moved in the proximity of it.
         mouse.x.set(center.x - cursorSize / 2);
         mouse.y.set(center.y - cursorSize / 2);
       } else {
+        // If it's not hovered, stick the cursor to the
+        // pointer around the page.
         mouse.x.set(clientX - cursorSize / 2);
         mouse.y.set(clientY - cursorSize / 2);
       }
     }
+    const handleMouseOver = () => setHovered(true);
+    const handleMouseLeave = () => setHovered(false);
 
-    function manageMouseOver() {
-      setHovered(true);
-    }
-
-    function manageMouseLeave() {
-      setHovered(false);
-    }
-    stickyElement.current.addEventListener("mouseenter", manageMouseOver);
-    stickyElement.current.addEventListener("mouseleave", manageMouseLeave);
-    window.addEventListener("mousemove", manageMouseMove);
+    stickyElement.current.addEventListener("mouseenter", handleMouseOver);
+    stickyElement.current.addEventListener("mouseleave", handleMouseLeave);
+    window.addEventListener("mousemove", handleMouseMove);
     return () => {
-      stickyElement.current.removeEventListener("mouseenter", manageMouseOver);
-      stickyElement.current.removeEventListener("mouseleave", manageMouseLeave);
-      window.removeEventListener("mousemove", manageMouseMove);
+      stickyElement.current.removeEventListener("mouseenter", handleMouseOver);
+      stickyElement.current.removeEventListener("mouseleave", handleMouseLeave);
+      window.removeEventListener("mousemove", handleMouseMove);
     };
   }, [cursorSize, hovered, mouse.x, mouse.y, stickyElement]);
 
