c5ec702a7cd6a0c60da4b8b3bde52b0751365f7c
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-27 20:32:04 +0530

    Magnetic: Move the burger along with the sticky cursor as well

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,15 @@
 import { forwardRef } from "react";
 import styles from "./style.module.scss";
+import Magnetic from "@/components/magnetic";
 
 const Header = forwardRef(function index(_, ref) {
   return (
     <div className={styles.header}>
-      <div className={styles.burger}>
-        <div ref={ref} className={styles.bounds} />
-      </div>
+      <Magnetic>
+        <div className={styles.burger}>
+          <div ref={ref} className={styles.bounds} />
+        </div>
+      </Magnetic>
     </div>
   );
 });
